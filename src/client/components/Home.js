fe97e66eada53fb12068826ad5f13779bf6fe4af
harry <harry@onlinebizsoft.com>
2018-09-03 21:14:18 +0700

    initial boilerplate for ssr react

---
+++ src/client/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <div>
+            <div>Hello world</div>
+            <button onClick={() => console.log('click')}>Click me</button>
+        </div>
+    );
+};
+
+export default Home;
